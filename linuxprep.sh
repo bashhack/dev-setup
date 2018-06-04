9ccd728f3009cec34502ee5debdeafac0b9b2428
Marc Laughton <info@marclaughton.com>
2018-06-03 23:17:18 -0400

    Add basic linuxprep

---
+++ linuxprep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ask for the admin password
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `linuxprep.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Step 1
+# Install all available updates
+apt update -y
+apt upgrade -y
