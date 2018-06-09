5ee0bdf4308c8311699b131c2b086c01eb9d0b42
Marc Laughton <info@marclaughton.com>
2018-06-09 17:52:09 -0400

    adjust pydev script

--- pydev.sh
+++ pydev.sh
@@ -10,16 +10,16 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.exports
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.exports
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
-echo "$SHELL"
 echo ""
 echo "------------------------------"
-echo "Installing pyenv versions: 3.6.5, 3.7-dev"
+echo "Installing pyenv versions: 3.6.5, 3.7-dev, and 3.8-dev"
 echo "------------------------------"
+. ~/.exports
+. ~/.bash_profile
 pyenv install 3.6.5
 pyenv install 3.7-dev
 pyenv install 3.8-dev
 pyenv global 3.6.5
-echo "$SHELL"
 
 # TODO: Install global code quality tools
 pip install python-language-server
@@ -31,3 +31,11 @@ pip install autoflake
 pip install isort
 pip install importmagic
 pip install epc
+
+echo ""
+echo "------------------------------"
+echo "Global Python version set"
+echo "------------------------------"
+
+which python
+pyenv versions
