21073f9212dc47a6d511e78c47645c76c4fd9781
bashhack (Marc Laughton) <info@marclaughton.com>
2018-06-15 08:43:02 -0400

    ad i3 and polybar configs

--- linuxprep.sh
+++ linuxprep.sh
@@ -55,28 +55,66 @@ echo "------------------------------"
 echo "Installing i3-gaps and configuring DM."
 echo "------------------------------"
 
-sudo apt install libxcb1-dev
-sudo apt install libxcb-keysyms1-dev
-sudo apt install libpango1.0-dev
-sudo apt install libxcb-util0-dev
-sudo apt install libxcb-icccm4-dev
-sudo apt install libyajl-dev
-sudo apt install libstartup-notification0-dev
-sudo apt install libxcb-randr0-dev
-sudo apt install libev-dev
-sudo apt install libxcb-cursor-dev
-sudo apt install libxcb-xinerama0-dev
-sudo apt install libxcb-xkb-dev
-sudo apt install libxkbcommon-dev
-sudo apt install libxkbcommon-x11-dev
-sudo apt install autoconf
-sudo apt install libxcb-xrm0
-sudo apt install libxcb-xrm-dev
-sudo apt install automake
-sudo apt install xutils-dev libtool
-sudo apt install i3status
+sudo apt install libxcb1-dev -y
+sudo apt install libxcb-keysyms1-dev -y
+sudo apt install libpango1.0-dev -y
+sudo apt install libxcb-util0-dev -y
+sudo apt install libxcb-icccm4-dev -y
+sudo apt install libyajl-dev -y
+sudo apt install libstartup-notification0-dev -y
+sudo apt install libxcb-randr0-dev -y
+sudo apt install libev-dev -y
+sudo apt install libxcb-cursor-dev -y
+sudo apt install libxcb-xinerama0-dev -y
+sudo apt install libxcb-xkb-dev -y
+sudo apt install libxkbcommon-dev -y
+sudo apt install libxkbcommon-x11-dev -y
+sudo apt install autoconf -y
+sudo apt install libxcb-xrm0 -y
+sudo apt install libxcb-xrm-dev -y
+sudo apt install automake -y
+sudo apt install xutils-dev libtool -y
+sudo apt install i3status -y
+sudo apt install rofi -y
+sudo apt install scrot -y
+sudo apt install gimp -y
+sudo apt install i3lock -y
 
-git clone https://www.github.com/Airblader/i3 i3-gaps
+# polybar
+sudo apt install cmake-data -y
+sudo apt install libcairo2-dev -y
+sudo apt install libxcb1-dev -y
+sudo apt install libxcb-ewmh-dev -y
+sudo apt install libxcb-icccm4-dev -y
+sudo apt install libxcb-image0-dev -y
+sudo apt install libxcb-randr0-dev -y
+sudo apt install libxcb-util0-dev -y
+sudo apt install pkg-config -y
+sudo apt install python-xcbgen -y
+sudo apt install xcb-proto -y
+sudo apt install libxcb-xrm-dev -y
+sudo apt install libxcb-cursor-dev -y
+sudo apt install libxcb-xkb-dev -y
+sudo apt install i3-wm -y
+sudo apt install libjsoncpp-dev -y
+sudo apt install libasound2-dev -y
+sudo apt install libpulse-dev -y
+sudo apt install libmpdclient-dev -y
+sudo apt install libiw-dev -y
+sudo apt install libcurl4-openssl-dev -y
+sudo apt install feh -y
+sudo apt install compton -y
+sudo apt install unifont -y
+sudo apt install fonts-font-awesome -y
+
+git clone --recursive https://github.com/jaagr/polybar
+mkdir polybar/build
+cd polybar/build
+cmake ..
+sudo make install
+cd ~
+
+# TODO: make script executable for chmod +x bar.sh spoty.sh
 cd i3-gaps/
 autoreconf --force --install
 rm -rf build/
@@ -85,3 +123,9 @@ mkdir -p build && cd build/
 make
 sudo make install
 cd ~
+
+cp init/i3/config ~/.config/i3/
+cp init/i3/spoty.sh ~/.config/i3/
+cp init/i3/bar.sh ~/.config/i3/
+cp init/polybar/config ~/.config/polybar/
+cp init/vapor.jpg ~/Pictures/
